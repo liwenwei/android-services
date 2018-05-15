4e44bf7e346b9e8d019cb22e1e3a89a3666979bf
FAREAST\v-wenwli <v-wenwli@microsoft.com>
2018-05-15 17:32:55 +0800

    Notification

--- app/src/main/java/com/liwenwei/android/servicedemo/MainActivity.java
+++ app/src/main/java/com/liwenwei/android/servicedemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.liwenwei.android.servicedemo;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     public void startMyService(View v) {
         Intent intent = new Intent(this, HelloService.class);
         startService(intent);
+    }
+
+    public void startMyNotification(View v) {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(this)
+                .setContentTitle("Some Message")
+                .setContentText("You've received new messages!")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
     }
 }
